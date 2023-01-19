1ab88c3b8a4489051a479687b8155f0006d2fb7b
PalomaBtz <paloma.benitez@intersel.com.mx>
2023-01-18 17:21:35 -0700

    fin sección 5

---
+++ components/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,20 @@
+import { Grid } from "@nextui-org/react";
+import React, { FC } from "react";
+import { FavoriteCardPokemon } from "./";
+
+interface Props {
+  favoritePokemons: number[];
+}
+
+export const FavoritePokemons: FC<Props> = ({ favoritePokemons }) => {
+  return (
+    <Grid.Container gap={2} direction="row" justify="flex-start">
+      {
+        ///Object.values(favoritePokemons)
+        favoritePokemons.map((id) => (
+          <FavoriteCardPokemon key={id} pokemonId={id} />
+        ))
+      }
+    </Grid.Container>
+  );
+};
